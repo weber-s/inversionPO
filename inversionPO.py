df9f50e5f2a9d45612107aab75fcdd319c23b9ff
Samuel <samuel.weber@normalesup.org>
2017-02-21 16:56:46 +0100

    Ok pour dataFrame

--- inversionPO.py
+++ inversionPO.py
@@ -45,6 +45,24 @@ def to_date(strdate):
     date = np.array(date)
     return(date)
 
+def sourcesColor():
+    color ={
+        "Vehicular": '#000000',
+        "Bio. burning": '#92d050',
+        "Sulfate-rich": '#ff2a2a',
+        "Nitrate-rich": '#ff7f2a',
+        "Secondary bio": '#8c564b',
+        "Sea/road salt": '#00b0f0',
+        "Primary bio": '#ffc000',
+        "Mineral dust": '#e9ddaf',
+        "AOS/dust": '#e9ddaf',
+        "Industrial": '#7030a0',
+        "Débris végétaux": '#2aff80',
+        "Chlorure": '#80e5ff'
+    }
+    color = pd.DataFrame(index=["color"], data=color)
+    return color
+
 def solveLinear(G,d,C):
     """
     Solve the linear system Gm=d with a covariance matrix of the parameters C
@@ -82,9 +100,9 @@ def loadCHEM(conc, fromSource, PMother=False, factor2regroup=[]):
     subset of them are used.
     """
     if fromSource:
-        exclude = ["POAAm3","POAAµg","PODTTm3","PODTTµg"]
-        CHEM    = conc.drop(exclude, axis=1, errors="ignore")
-        #CHEM    = conc
+        #exclude = ["POAAm3","POAAµg","PODTTm3","PODTTµg"]
+        #CHEM    = conc.drop(exclude, axis=1, errors="ignore")
+        CHEM    = conc
         if PMother:
             try:
                 All     = ["Sea/road salt","Secondary bio","Primary bio",\
@@ -107,58 +125,78 @@ def loadCHEM(conc, fromSource, PMother=False, factor2regroup=[]):
 def reversePO(conc, unc, PO, fromSource):
     #date=to_date(conc.index)
     # ===== CHEM PART
-    CHEM    = loadCHEM(conc, fromSource)
-    label   = CHEM.columns
-    CHEM    = CHEM.as_matrix()
-    # ===== PO PART
-    PO      = conc[POtype]#+conc["POAAµg"]
-    PO      = PO.as_matrix()
-    POunc   = unc[POtype]
-    POunc   = POunc.as_matrix()
-    # ===== Ensure no NaN + build covariance matrix
-    idok    = ~np.isnan(PO)
-    G       = CHEM[idok,:]
-    d       = PO[idok]
-    Covd    = np.diag(np.power(POunc[idok],2))
-    # ===== Linear inversion
-    m, Covm, Resm= solveLinear(G,d,Covd)
     #CHEM    = loadCHEM(conc, fromSource)
-    #rowOk   = CHEM.T.notnull().all()
-    #colKo   = ["POAAm3","POAAµg","PODTTm3","PODTTµg"]
-    #m, Covm, Resm= solveLinear(G=CHEM.ix[rowOk,:].drop(colKo,
-    #                                                   axis=1,
-    #                                                   errors="ignore").as_matrix(),
-    #                           d=CHEM.ix[rowOk,POtype].as_matrix(),
-    #                           C=Covd)
+    #label   = CHEM.columns
+    #CHEM    = CHEM.as_matrix()
+    # ===== PO PART
+    #PO      = conc[POtype]#+conc["POAAµg"]
+    #PO      = PO.as_matrix()
+    #POunc   = POunc.as_matrix()
+    # ===== Ensure no NaN + build covariance matrix
+    #idok    = ~np.isnan(PO)
+    #G       = CHEM[idok,:]
+    #d       = PO[idok]
+    #Covd    = np.diag(np.power(POunc[idok],2))
+    # ===== Linear inversion
+    #m, Covm, Resm= solveLinear(G,d,Covd)
+    CHEM    = loadCHEM(conc, fromSource)
+    POunc   = unc[POtype]
+    rowOkV  = CHEM.T.notnull().all()
+    rowOkPO = POunc.T.notnull()
+    rowOk   = rowOkV & rowOkPO
+    colKo   = ["POAAm3","POAAµg","PODTTm3","PODTTµg","POPerCent"]
+    G       = CHEM.ix[rowOk,:].drop(colKo,
+                                    axis=1,
+                                    errors="ignore")
+    d       = CHEM.ix[rowOk,POtype]
+    Covd    = np.diag(np.power(POunc[rowOk],2))
+    m, Covm, Resm= solveLinear(G=G.as_matrix(),
+                               d=d.as_matrix(),
+                               C=Covd)
+    m = pd.DataFrame(index=CHEM.drop(colKo,axis=1,errors="ignore").columns, columns=[name], data=m)
     # ===== model reconstruction
-    model       = np.dot(G,m)
-    residu      = np.dot((d-np.dot(G,m)).T,(d-np.dot(G,m)))
-    p   = polyfit(d,model,1)
-    r2  = np.corrcoef(np.vstack((d,model)))
+    model       = CHEM.drop(colKo,axis=1,errors="ignore").dot(m)
+    residu      = (d - model.ix[rowOk,0]).T.dot( d - model.ix[rowOk,0])
+    p   = polyfit(d,model.ix[rowOk,:],1)
+    p.shape = (2,)
+    r2  = pd.concat([d,model],axis=1).corr()
     # ===== Save result in a dictionary
     station             = {}
     station[POtype]     = conc[POtype]
-    station['CHEM']     = loadCHEM(conc, fromSource)
+    station['CHEM']     = CHEM
     station['m']        = m
     station['G']        = G
-    #station['model']    = pd.DataFrame(index=station['PO'].index,data=np.dot(G,m))
+    station['model']    = model
     station['d']        = d
     station['r2']       = r2
     station['p']        = p
     station['Covm']     = Covm
     station['Resm']     = Resm
     station['residu']   = residu
-    station['label']    = label
     return station
 
 
 
-def plot_coeff(stoModel, POtype_list, fromSource=True, saveFig=False):
+def plot_coeff(station, POtype_list, fromSource=True, saveFig=False):
     """Plot a bar plot of the intrinsique PO of the sources for all the station"""
     f,ax=plt.subplots(nrows=len(POtype_list),ncols=1,sharex=True,figsize=(17,8))
     for i, POtype in enumerate(POtype_list):
-        stoModel[POtype][0].plot(kind="bar", yerr=stoModel[POtype][1],
-                                 ax=ax[i], legend=False)
+        dfVal = pd.DataFrame()
+        dfUnc = pd.DataFrame()
+        for name in station.keys():
+            mValtmp = pd.DataFrame(index=station[name][POtype]["G"].columns,
+                                   data=station[name][POtype]["m"],
+                                   columns=[name])
+            mUnctmp = pd.DataFrame(index=station[name][POtype]["G"].columns,
+                                   data=np.sqrt(np.diag(station[name][POtype]['Covm'])),
+                                   columns=[name])
+            dfVal = pd.merge(dfVal, mValtmp, right_index=True, left_index=True,
+                               how="outer")
+            dfUnc  = pd.merge(dfUnc, mUnctmp, right_index=True, left_index=True,
+                               how="outer")
+
+        dfVal.plot(kind="bar", yerr=dfUnc, ax=ax[i], legend=False)
+
         ax[i].set_title(POtype)
         ax[i].set_ylabel("nmol/min/µg")
         plt.subplots_adjust(top=0.95, bottom=0.16, left=0.07, right=0.93)
@@ -171,23 +209,31 @@ def plot_coeff(stoModel, POtype_list, fromSource=True, saveFig=False):
                 plt.savefig("figures/svg/coeffAllSites.svg")
                 plt.savefig("figures/pdf/coeffAllSites.pdf")
 
-def plot_contribPie(stoModel, POtype_list, saveFig=False):
+def plot_contribPie(station, POtype_list, saveFig=False):
     """Plot contribution of the PO in a Pie chart"""
     f,ax=plt.subplots(nrows=len(POtype_list),ncols=len(station_list),figsize=(17,8))
     for j,name in enumerate(station_list):
         for i, POtype in enumerate(POtype_list):
-            param   = stoModel[POtype][0][name]
-            conc    = concentrations[name]
-            df      = conc*param
+            param   = station[name][POtype]["m"]
+            conc    = station[name][POtype]["G"]
+            df      = conc*param.T.iloc[0]
             if (np.sum(df.dropna(axis=1))<0).any():
                 continue
-        np.sum(df.dropna(axis=1)).plot.pie(ax=ax[i][j])
+        ax[i][j].set_aspect('equal')
+        df.sort_index(axis=1,inplace=True)
+        c = sourcesColor()
+        cols = c.ix["color",df.columns].values
+        np.sum(df.dropna(axis=1)).plot.pie(ax=ax[i][j],
+                                           shadow=False,
+                                           startangle=90,
+                                           colors=cols)
+        ax[i][j].set_title(name)
+        ax[i][j].set_ylabel("")
         plt.subplots_adjust(top=0.95, bottom=0.16, left=0.07, right=0.93)
-        plt.legend(loc="center", bbox_to_anchor=(0.5,-0.3), ncol=len(station_list))
-        if saveFig:
-            plt.savefig("figures/coeffAllSites.png")
-            plt.savefig("figures/svg/coeffAllSites.svg")
-            plt.savefig("figures/pdf/coeffAllSites.pdf")
+    if saveFig:
+        plt.savefig("figures/coeffAllSites.png")
+        plt.savefig("figures/svg/coeffAllSites.svg")
+        plt.savefig("figures/pdf/coeffAllSites.pdf")
 
 
 mpl.rcParams.update({'font.size': 12})
@@ -199,10 +245,10 @@ fileDirPO   = "/home/samuel/Documents/IGE/BdD_PO/"
 fromSource  = True
 
 
-station_list= ("Frenes","Passy","Marnaz","Chamonix")
-POtype_list = ["POAAm3","PODTTm3"]
+station_list= ("Passy","Marnaz","Chamonix")
+POtype_list = ["POPerCent"]
 
-plotBool    = False
+plotBool    = True
 saveFig     = False
 
 if fromSource:
@@ -221,90 +267,86 @@ concentrations, uncertainties = loadData(station_list, POtype_list,
                                          BasefileConc, BasefileUnc)
 
 # ========= INVERSION ========================================================
-for POtype in POtype_list:
-    for name in station_list:
+for name in station_list:
+    station[name]   = {}
+    for POtype in POtype_list:
         print("\n===== {name} for {PO} =====".format(name=name, PO=POtype))
-        station[name] =  reversePO(concentrations[name],
-                                   uncertainties[name],
-                                   POtype,
-                                   fromSource)
+        station[name][POtype] =  reversePO(concentrations[name],
+                                           uncertainties[name],
+                                           POtype,
+                                           fromSource)
         # plot part ==================================================================
 
         print("\nResolution param:")
-        print(np.array_str(station[name]['Resm'],suppress_small=True))
+        print(np.array_str(station[name][POtype]['Resm'],suppress_small=True))
         #for i, x in enumerate(m):
             #    print("%s \t%0.4f\t± %0.4f" % (concentrations[name].keys()[i], x,
             #                                    np.sqrt(concentrations[name].Covm[i,i])))
 
         if plotBool:
+            CHEM= station[name][POtype]["CHEM"]
+            PO  = station[name][POtype][POtype]
+            model = station[name][POtype]["model"]
+            m   = station[name][POtype]["m"]
+            p   = station[name][POtype]["p"]
+            r2  = station[name][POtype]["r2"].as_matrix()
+            Covm= station[name][POtype]["Covm"]
             plt.figure(figsize=(17,8))
             # time serie reconstruction/observation
             ax=plt.subplot(2,3,(1,3))
-            plt.plot(date,PO, "o-b", label="Obs.")
-            plt.plot(date,np.dot(CHEM,m), "*-r", label="recons.")
-            plt.title("{station} PO{PO}".format(station=name, PO=POtype))
-            l=plt.legend()
+            PO.plot(ax=ax, style="b-o", label="Obs.")
+            model.name = "recons."
+            model.plot(ax=ax, style="r-*", label="recons.", rot=0)
+            plt.title("{station} {PO}".format(station=name, PO=POtype))
+            l=ax.legend(('Obs.','Recons.'))
             l.draw_frame(False)
             # scatter plot reconstruction/observation
             ax=plt.subplot(2,3,4)
-            plt.scatter(PO,np.dot(CHEM,m))
-            plt.plot([0,np.nanmax(PO)],[0, np.nanmax(PO)], '--', label="y=x")
-            plt.plot([0,np.nanmax(PO)],[p[1], p[0]*np.nanmax(PO)+p[1]], label="linear fit")
+            pd.concat((PO,model), axis=1).plot(ax=ax,x=[0], y=[1], kind="scatter")
+            plt.plot([0,PO.max()],[0, PO.max()], '--', label="y=x")
+            plt.plot([0,PO.max()],[p[1], p[0]*PO.max()+p[1]], label="linear fit")
             posy = 0.7*plt.ylim()[1]
             plt.text(0,posy,"y=%.2fx+%0.2f\nr²=%0.2f" % (p[0],p[1],r2[0,1]))
             plt.xlabel("PO obs.")
             plt.ylabel("PO reconstruct")
             plt.title("obs. vs reconstruction")
+            ax.set_aspect(1./ax.get_data_ratio())
             l=plt.legend(loc="lower right")
             l.draw_frame(False)
             # factors contribution
             ax=plt.subplot(2,3,5)
-            plt.bar(range(0,len(m)),m,
-                    yerr=np.sqrt(np.diag(Covm)),
-                    ecolor="k",
-                    align='center')
-            ax.set_xticks(range(0,len(m)))
-            ax.set_xticklabels(label, rotation=-60)
+            m.plot(ax=ax,
+                   kind="bar",
+                   yerr=np.sqrt(np.diag(Covm)),
+                   ecolor="k",
+                   align='center',
+                   rot=-60,
+                   legend=False)
             plt.ylabel("PO [nmol/min/µg]")
             #plt.ylim((-0.1,1.4))
             # Pie chart
             ax=plt.subplot(2,3,6)
             ax.set_aspect('equal')
-            plt.pie(np.sum(CHEM*m,axis=0),labels=label)
+            df = CHEM.ix[:,m.index] * m.T.iloc[0]
+            if (df.sum()>0).all():
+                df.sum().plot.pie(shadow=False, startangle=90)
+                ax.set_ylabel("")
 
             plt.subplots_adjust(top=0.95, bottom=0.16, left=0.07, right=0.93)
 
             if saveFig:
-                plt.savefig("figures/svg/inversion"+name+"PO"+POtype+".svg")
-                plt.savefig("figures/pdf/inversion"+name+"PO"+POtype+".pdf")
-                plt.savefig("figures/inversion"+name+"PO"+POtype+".png")
+                plt.savefig("figures/svg/inversion"+name+POtype+".svg")
+                plt.savefig("figures/pdf/inversion"+name+POtype+".pdf")
+                plt.savefig("figures/inversion"+name+POtype+".png")
 
             #plt.tight_layout(h_pad=0.4)
 
-    mValues= pd.DataFrame()
-    mUnc   = pd.DataFrame()
-    for name in station_list:
-        mValtmp = pd.DataFrame(index=station[name]["label"],
-                               data=station[name]["m"],
-                               columns=(name,))
-        mUnctmp = pd.DataFrame(index=station[name]['label'],
-                               data=np.sqrt(np.diag(station[name]['Covm'])),
-                               columns=(name,))
-        mValues = pd.merge(mValues, mValtmp, right_index=True, left_index=True,
-                           how="outer")
-        mUnc    = pd.merge(mUnc, mUnctmp, right_index=True, left_index=True,
-                           how="outer")
-
-    stoModel[POtype]=[mValues, mUnc]  
-
-
 # ========== PLOT COEFFICIENT =================================================
-plot_coeff(stoModel, POtype_list, fromSource=fromSource, saveFig=saveFig)
+plot_coeff(station, POtype_list, fromSource=fromSource, saveFig=saveFig)
 
 
 # ========== CONTRIBUTION PIE CHART ===========================================
-plot_contribPie(stoModel, POtype_list, saveFig=saveFig)
-
+plot_contribPie(station, POtype_list, saveFig=saveFig)
 
 
 
